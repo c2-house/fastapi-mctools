41037bf661ef5ce11fc3ffd28be596055b8c6632
Jungminchae <minchae3618@gmail.com>
2023-10-14 22:36:05 +0900

    Add sqlalchemy db session

---
+++ fastapi_mctools/db/sqlalchemy.py
@@ -0,0 +1,46 @@
+from sqlalchemy import MetaData
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
+
+
+class DB:
+    """
+    db connection by sqlalchemy
+    get_db = DB(db_url)
+    """
+
+    def __init__(self, db_url: str) -> None:
+        self.engine = create_engine(db_url, pool_pre_ping=True)
+        self.session = sessionmaker(autocommit=False, autoflush=False, bind=self.engine)
+
+    def __call__(self) -> Session:
+        db = self.session()
+        try:
+            yield db
+        finally:
+            db.close()
+
+
+class Async_DB:
+    """
+    db connection by sqlalchemy
+    get_db = Async_DB(db_url)
+    """
+
+    def __init__(self, db_url: str) -> None:
+        self.engine = create_async_engine(db_url)
+        self.async_session = async_sessionmaker(
+            self.engine, autoflush=False, autocommit=False
+        )
+        self.meta = MetaData()
+
+    async def __call__(self) -> AsyncSession:
+        async with self.engine.begin() as conn:
+            await conn.run_sync(self.meta.create_all)
+
+        db = self.async_session()
+        try:
+            yield db
+        finally:
+            await db.close()
