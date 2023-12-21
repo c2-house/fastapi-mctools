8aa6f83f87d4a7526091f1a67cf8626e1c332a83
Jungminchae <minchae3618@gmail.com>
2023-12-21 11:00:28 +0900

    Add handle http exception

--- fastapi_mctools/exceptions.py
+++ fastapi_mctools/exceptions.py
@@ -1,9 +1,11 @@
-from fastapi import HTTPException as FastAPIHTTPException, status
+from typing import ParamSpecKwargs
+from fastapi import HTTPException as FastAPIHTTPException, Request, status
+from fastapi.responses import JSONResponse
 
 
 class HTTPException(FastAPIHTTPException):
     """
-    Custom HTTPException class to add a code field to the response.
+    Custom HTTPException class
     ...
     raise HTTPException(
         status_code=404,
@@ -16,11 +18,30 @@ class HTTPException(FastAPIHTTPException):
         self,
         status_code: int,
         detail: str = None,
-        code: str = None,
         headers: dict = None,
+        **kwargs: ParamSpecKwargs,
     ):
         super().__init__(status_code, detail, headers)
-        self.code = code
+        for key, value in kwargs.items():
+            setattr(self, key, value)        
+
+    @property
+    def attributes(self) -> dict:
+        self.__dict__.pop("status_code")
+        self.__dict__.pop("headers")
+        return self.__dict__
+
+
+async def handle_http_exception(request: Request, exc: HTTPException) -> JSONResponse:
+    """
+    HTTPException을 JSONResponse로 변환하여 반환합니다.
+    """
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=exc.attributes,
+        headers=exc.headers,
+    )
+
 
 
 async def exception_handler(error: Exception) -> HTTPException:
