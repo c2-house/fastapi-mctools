092d92919274059c1c8d6aba7b2a3fba0a4e53f0
Jungminchae <minchae3618@gmail.com>
2024-02-20 16:15:50 +0900

    Refactor ORMBase and AReadBase classes to improve code readability and maintainability

--- fastapi_mctools/orms/__init__.py
+++ fastapi_mctools/orms/__init__.py
@@ -5,8 +5,8 @@ T = TypeVar("T")
 
 class ORMBase(Generic[T]):
     """
-    ORMBase들은 Sqlalchemy의 Base를 넣어주는 역할을 합니다.
-    - model: Sqlalchemy의 Base를 상속받은 클래스
+    ORMBase classes serve the purpose of providing Sqlalchemy's Base.
+    - model: A class that inherits from Sqlalchemy's Base.
     """
 
     def __init__(self, model: T):
@@ -14,7 +14,7 @@ class ORMBase(Generic[T]):
 
     def get_columns(self, columns: list[str] | None = None) -> list:
         """
-        columns에 해당하는 컬럼들을 가져옵니다.
+        Retrieves the columns specified in the 'columns' parameter.
         """
         if columns:
             return [getattr(self.model, column) for column in columns]
@@ -22,8 +22,8 @@ class ORMBase(Generic[T]):
 
     def get_result(self, result: T, columns: list) -> T:
         """
-        Column 전체 일 때(*)는 scalar_one_or_none()을 사용하고,
-        몇 개의 Column을 선택할 때는 mappings().first()를 사용합니다.
+        When all columns (*) are selected, use scalar_one_or_none(),
+        When selecting a few columns, use mappings().first().
         """
         if len(columns) == 1:
             return result.scalar_one_or_none()
@@ -31,10 +31,30 @@ class ORMBase(Generic[T]):
 
     def get_results(self, results: list[T], columns: list) -> list[T]:
         """
-        Column 전체 일 때(*)는 scalars().all()을 사용하고,
-        몇 개의 Column을 선택할 때는 mappings().all()를 사용합니다.
+        When all columns (*) are selected, use scalars().all(),
+        When selecting a few columns, use mappings().all().
         """
 
         if len(columns) == 1:
             return results.scalars().all()
         return results.mappings().all()
+
+    def get_filters_by_operator(self, kwargs: dict, operator: str) -> list:
+        """
+        Creates filters based on the 'kwargs' parameter and the specified 'operator'.
+        """
+        match operator:
+            case "eq":
+                return [(getattr(self.model, k) == v) for k, v in kwargs.items() if hasattr(self.model, k)]
+            case "ne":
+                return [(getattr(self.model, k) != v) for k, v in kwargs.items() if hasattr(self.model, k)]
+            case "lt":
+                return [(getattr(self.model, k) < v) for k, v in kwargs.items() if hasattr(self.model, k)]
+            case "le":
+                return [(getattr(self.model, k) <= v) for k, v in kwargs.items() if hasattr(self.model, k)]
+            case "gt":
+                return [(getattr(self.model, k) > v) for k, v in kwargs.items() if hasattr(self.model, k)]
+            case "ge":
+                return [(getattr(self.model, k) >= v) for k, v in kwargs.items() if hasattr(self.model, k)]
+            case _:
+                raise ValueError("Invalid operator")
