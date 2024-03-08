510c38939c83357b75728babdfb7b4a70c39aa6f
Jungminchae <minchae3618@gmail.com>
2024-03-08 09:08:08 +0900

    Add dictionary-like behavior to ResponseInterFace class

--- fastapi_mctools/utils/responses.py
+++ fastapi_mctools/utils/responses.py
@@ -38,3 +38,15 @@ class ResponseInterFace:
             str: The response as a string.
         """
         return json.dumps(self.to_dict(), ensure_ascii=False)
+
+    def __getitem__(self, key: str) -> Union[ResultType, str]:
+        return self.to_dict()[key]
+
+    def __iter__(self):
+        return iter(self.to_dict())
+
+    def keys(self):
+        return self.to_dict().keys()
+
+    def items(self):
+        return self.to_dict().items()
