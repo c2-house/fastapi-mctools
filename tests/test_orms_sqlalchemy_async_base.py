7b1a89775c0810c17d8b436ea956c7f66f481503
Jungminchae <minchae3618@gmail.com>
2024-03-21 15:15:23 +0900

    Add Item model to Base class and stop supporting sync db test

--- tests/test_orms_sqlalchemy_async_base.py
+++ tests/test_orms_sqlalchemy_async_base.py
@@ -1,1 +1,23 @@
-import pytest  # noqa: F401
+import pytest
+from fastapi_mctools.orms.sqlalchemy.async_base import ACreateBase, AReadBase
+
+
+class TestAsyncBase:
+    @pytest.fixture
+    def orm_obj(self, item):
+        class Create(ACreateBase):
+            ...
+
+        class Read(AReadBase):
+            ...
+
+        class ORM(Read, Create):
+            def __init__(self, model):
+                self.model = model
+
+        return ORM(item)
+
+    @pytest.mark.asyncio
+    async def test_create(self, orm_obj, async_db):
+        item = await orm_obj.create(async_db, name="test")
+        assert item.name == "test"
